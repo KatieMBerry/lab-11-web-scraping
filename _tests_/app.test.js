e1eaef70716fefe462c75be5eb5af2525997d224
Katie <katieberrym@gmail.com>
2020-12-16 18:42:25 -0800

    added passing fake test to clear CI

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -1,2 +1,13 @@
+const request = require('supertest');
+const app = require('../lib/app');
 
+describe('app tests', () => {
+    it('responds with hello', () => {
+        return request(app)
+            .get('/')
+            .then(res => {
+                expect(res.text).toEqual('hello')
+            });
+    });
+});
 
